036f1c370d2cd36cf3f8227d3e6b2024ace88898
MANGELTT <160346710+MANGELTT@users.noreply.github.com>
2024-05-29 01:28:32 -0600

    Create MANGELTT.js
    1er avance

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/MANGELTT.js
@@ -0,0 +1,7 @@
+/* https://developer.mozilla.org/es/docs/Learn/JavaScript/First_steps/What_is_JavaScript */
+// Hola Liz tu no me conoces pro yo a ti si.
+
+const myName = 'Matt';
+
+
+let myAge = '22';
