943b8e996feaacf4210911c2865754a3f31b82b2
MANGELTT <160346710+MANGELTT@users.noreply.github.com>
2024-05-29 19:59:32 -0600

    MANGELTT.js
    Primer paso

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/MANGELTT.js
@@ -0,0 +1,25 @@
+// Comentario
+
+/* Multiples
+    Lines
+    GUAPO */
+
+//string
+let nombre = 'Miguel';
+//numero
+var number = 5;
+//numero grande
+let bigint = 8445647842348945484214984564654214465465454n;
+//Boolean
+let estaLloviendo = true;
+let seFueLaLuz = false;
+//Undifined
+let variableIndefinida;
+//nulo
+let valorNulo = null;
+//symbol
+let simboloUnico = Symbol('descripcion');
+
+
+
+console.log('Hola'+ nombre);
