c71e83f4f75acd7603bd2f11eabf7a34b27008cc
MANGELTT <torresmangel3@gmail.com>
2024-06-11 22:50:29 -0600

    Primer reto

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/MANGELTT.js
@@ -0,0 +1,18 @@
+//https://developer.mozilla.org/en-US/docs/Web/javascript
+
+//Comentario en una linea 
+
+/*Comentario 
+en multiples 
+lineas */
+
+let num = 69
+const pi = 3.1416
+
+let numeroGrande = 1234567890123456789n
+let texto = "Hola mundo"
+let booleano = true
+let indefinido;
+
+
+console.log("¡Hola, Javascript!")
---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/MANGELTT.js
@@ -0,0 +1,37 @@
+// Aritmeticos
+console.log("7 x 3 = " + 7 * 3);
+console.log(7 + 5);
+console.log(7%9);
+
+//Logicos
+let A = 8;
+let B = 10;
+
+if (!(A > B)){
+    console.log("Factos");
+}
+else{
+    console.log("No Factos");
+}
+
+console.log(B>A | B<A ? "Joto" : "Guapo");
+
+console.log(B>A & B<A ? "Joto" : "Guapo");
+
+
+//Identidad
+console.log(5 === 5);
+console.log(5 === '5');
+console.log(5 !== 5); 
+console.log(5 !== '5');
+console.log(5 == 5);
+console.log(5 == '5'); 
+console.log(5 != 5);
+console.log(5 != '6'); 
+
+
+for (let i = 0; i < 5; i++){
+    console.log("Hola cliente " + i);
+}
+
+console.log(~6);
